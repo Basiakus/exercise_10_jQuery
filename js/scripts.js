c783888e5ee82bcbf1b97209dcd0439c05f3d9a8
Basiakus <panadambasiak@gmail.com>
2017-11-12 13:14:03 +0100

    scripts.js formated

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,67 @@
 //ćwiczenia praktyczne
+$(function() {
+  /*
+    var p = $('<p></p>'),
+        pWithText = $('<p>Hi!</p>'),
+        pWithClass = $('<p class="greeting">Hi!</p>');
+    console.log(p);
+    console.log(pWithText);
+    console.log(pWithClass);
 
-$(function(){
- /* var p = $( '<p></p>' ),
-      pWithText = $( '<p>Hi!</p>' ),
-      pWithClass = $( '<p class="greeting">Hi!</p>' ); 
-  console.log(p);
-  console.log(pWithText);
-  console.log(pWithClass);
+    var guzik = $('<button class="btn btn-default">guzik</btton>')
 
-  var guzik = $( '<button class="btn btn-default">guzik</btton>' )
-  
-  $("body").append(pWithText);
-  $("body").append(pWithClass);
-  $("body").append(guzik);
+    $("body").append(pWithText);
+    $("body").append(pWithClass);
+    $("body").append(guzik);
 
 
-  $(pWithText).css('font-size', '32px');
-  $(guzik).css("padding","30px").css("border-radius","5px");
+    $(pWithText).css('font-size', '32px');
+    $(guzik).css("padding", "30px").css("border-radius", "5px");
 
-$('button').each(function(index, element) {
-	$(element).text("guzik " + index);
-}).css('padding', '30px');
-  
+    $('button').each(function(index, element) {
+        $(element).text("guzik " + index);
+    }).css('padding', '30px');
 
-$('li').each(function(index, element) {
-	$(element).text("element listy " + index);
-});
-var ul = $("ul");
-var firstUl = ul.eq(0);
-var secondUl = ul.eq(1);
-var thridUl = ul.eq(2);
-firstUl.css("color","blue");
-secondUl.css("color", "red");
-thridUl.css("color","violet");
 
-var firstLifirstUl = $(firstUl).find("li").first().css("color","black");
-var secondLisecondUl = $(firstUl.next()).find("li").eq(1).css("color","black");
-var thirdUlThirdLi = $(ul.eq(2)).find("li").eq(2).css("color","black");
+    $('li').each(function(index, element) {
+        $(element).text("element listy " + index);
+    });
+    var ul = $("ul");
+    var firstUl = ul.eq(0);
+    var secondUl = ul.eq(1);
+    var thridUl = ul.eq(2);
+    firstUl.css("color", "blue");
+    secondUl.css("color", "red");
+    thridUl.css("color", "violet");
 
-var addLi = $(firstUl).append("<li>dodatkowy element</li>");
-$(firstUl).find("li").eq(3).css("color", "yellow").text("yellow");
-*/
+    var firstLifirstUl = $(firstUl).find("li").first().css("color", "black");
+    var secondLisecondUl = $(firstUl.next()).find("li").eq(1).css("color", "black");
+    var thirdUlThirdLi = $(ul.eq(2)).find("li").eq(2).css("color", "black");
 
-//zadanie 10.1
-var jqSpan = $('span');
-jqSpan.each(function(index, element) {
-    if (index % 2 == 0) {
-        $(element).css("color", "red")
-    } else {
-        $(element).css("color", "blue")
-    }
-});
+    var addLi = $(firstUl).append("<li>dodatkowy element</li>");
+    $(firstUl).find("li").eq(3).css("color", "yellow").text("yellow");
+    
+    */
 
-var paragraf = $('p');
+    //zadanie 10.1
+    var jqSpan = $('span');
+    jqSpan.each(function(index, element) {
+        if (index % 2 == 0) {
+            $(element).css("color", "red");
+        } else {
+            $(element).css("color", "blue");
+        }
+    });
 
-$(paragraf).each(function(index, element) {
-    var buttonWithAttr = $('<button data-tmp="' + index + '">PRZYCISK</button>');
-    $(element).append(buttonWithAttr);
-});
+    var paragraf = $('p');
 
-$('button').click(function() {
-alert($(this).attr("data-tmp"));
-});
+    $(paragraf).each(function(index, element) {
+        var buttonWithAttr = $('<button data-tmp="' + index + '">PRZYCISK</button>');
+        $(element).append(buttonWithAttr);
+    });
+
+    $('button').click(function() {
+        alert($(this).attr("data-tmp"));
+    });
 
 });
